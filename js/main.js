a5470648fbb1e45af7753ecf8580082e3f8a0cb9
Vlad <vladobradley@yahoo.com>
2022-04-11 21:45:33 +0600

    CROP once <p>

--- js/main.js
+++ js/main.js
@@ -1,6 +1,38 @@
-"use strict"
+"use strict";
 
-$(document).ready(function() {
-     
-});
+let startText = document.getElementsByClassName('crop-text')[0];
+let getStartText = startText.innerText;
+let cropText = getStartText.slice(0, 200);
 
+let btnMore = document.createElement('a');
+btnMore.classList.add("btn-more");
+btnMore.innerHTML = 'Подробнее...';
+
+let btnHide = document.createElement('a');
+btnHide.classList.add("btn-hide");
+btnHide.innerHTML = 'Скрыть...';
+
+console.log(startText);
+console.log(getStartText);
+console.log(cropText);
+
+function fullText() {
+  startText.innerHTML = getStartText;
+  startText.innerHTML = getStartText;
+  startText.append(btnHide);
+}
+
+function hideText() {
+  startText.innerHTML = cropText;
+  startText.append(btnMore);
+}
+
+if (getStartText.length > 100) {
+  startText.innerHTML = cropText;
+  startText.append(btnMore);
+}
+
+btnMore.onclick = fullText;
+btnHide.onclick = hideText;
+
+console.log(cropText);
