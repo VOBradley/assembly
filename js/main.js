19fde096ac1cd51e265d8c46893a99404cf227f8
Vlad <vladobradley@mail.ru>
2022-05-02 22:20:35 +0600

    for Key Function

--- js/main.js
+++ js/main.js
@@ -1,6 +1,25 @@
-"use strict"
+const cities = [
+	{
+		name: 'Москва',
+		population: 10000,
+	},
+	{
+		name: 'Санкт-Петербург',
+		population: 20000,
+	},
+	{
+		name: 'Екатеринбург',
+		population: 30000,
+	},
+]
 
-$(document).ready(function() {
-     
-});
+function getCities(obj) {
+	for (let i = 0; i < obj.length; i++) {
+    console.log({...obj[i]});
+		for (let key in obj[i]) {
+			console.log(key + " = " + obj[i][key])
+		}
+	}
+}
 
+getCities(cities)
